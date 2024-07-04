688b9b4f27f734f7337f190b701415e4796f2e11
leandrocgsi <leandrocgsi@gmail.com>
2024-07-04 18:39:04 -0300

    Initial Commit

--- src/main/java/br/com/erudio/environment/InstanceInformationService.java
+++ src/main/java/br/com/erudio/environment/InstanceInformationService.java
@@ -10,7 +10,6 @@ public class InstanceInformationService {
 
     private static final String DEFAULT_ENV_INSTANCE_GUID = "LOCAL";
 
-    // @Value(${ENVIRONMENT_VARIABLE_NAME:DEFAULT_VALUE})
     @Value("${" + HOST_NAME + ":" + DEFAULT_ENV_INSTANCE_GUID + "}")
     private String hostName;
 
