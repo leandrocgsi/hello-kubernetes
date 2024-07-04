4cae572790930f47ffc0c2d4a3e27d483153b243
leandrocgsi <leandrocgsi@gmail.com>
2024-07-04 18:24:30 -0300

    Initial Commit

--- src/main/java/br/com/erudio/HelloWorldController.java
+++ src/main/java/br/com/erudio/HelloWorldController.java
@@ -2,10 +2,9 @@ package br.com.erudio;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.in28minutes.rest.webservices.restfulwebservices.environment.InstanceInformationService;
+import br.com.erudio.environment.InstanceInformationService;
 
 @RestController
 public class HelloWorldController {
