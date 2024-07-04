c0c14a49fc8a08e913c891a5dbce73170ba0b803
leandrocgsi <leandrocgsi@gmail.com>
2024-07-04 18:17:46 -0300

    Initial Commit

--- src/main/java/br/com/erudio/Startup.java
+++ src/main/java/br/com/erudio/Startup.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HelloKubernetesApplication {
+public class Startup {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloKubernetesApplication.class, args);
+		SpringApplication.run(Startup.class, args);
 	}
 
 }
